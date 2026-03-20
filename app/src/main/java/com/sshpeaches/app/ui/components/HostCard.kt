ab1abd169dcef77c3049db10a3a698c4294b468c
Ali Sherief <ali@zenulabidin.com>
2026-03-20 18:37:56 +0300

    Use linux fallback icon for detected distros

--- app/src/main/java/com/sshpeaches/app/ui/components/HostCard.kt
+++ app/src/main/java/com/sshpeaches/app/ui/components/HostCard.kt
@@ -585,12 +585,14 @@ private fun OsMetadata.iconResOrNull(): Int? = when (this) {
         OsFamily.MINT -> R.raw.linux_mint
         OsFamily.ARCH -> R.raw.arch
         OsFamily.SUSE -> R.raw.suse
-        OsFamily.REDHAT -> R.raw.redhat
+        OsFamily.REDHAT,
+        OsFamily.CENTOS,
+        OsFamily.ROCKY,
+        OsFamily.ALMA -> R.raw.redhat
         OsFamily.MAC -> R.raw.apple
         OsFamily.WINDOWS -> R.raw.windows
         OsFamily.BSD -> R.raw.bsd
-        OsFamily.GENERIC -> R.raw.linux
-        else -> null
+        else -> R.raw.linux
     }
     else -> null
 }
